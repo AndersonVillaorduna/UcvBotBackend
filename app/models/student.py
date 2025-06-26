ce359ccee745df8b5ef64d471aee515eaffb4f91
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-06-26 00:50:11 -0500

    fix db postgre

--- app/models/student.py
+++ app/models/student.py
@@ -1,11 +1,11 @@
 from mini_db.conexion import conectar_db
-
+import psycopg2.extras
 def getAll():
     conexion = conectar_db()
     if not conexion:
         return None
     try:
-        cursor = conexion.cursor(dictionary=True)
+        cursor = conexion.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute("SELECT * FROM student")
         return cursor.fetchall()
     finally:
