7db8de429ffcc73ac716573b09479ff44b31e673
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-06-26 18:33:28 -0500

    fix import, delete @cross_origin

--- app/routes/level.py
+++ app/routes/level.py
@@ -1,29 +1,23 @@
 from flask import Blueprint, request, jsonify
-from flask_cors import cross_origin
 from mini_db.conexion import conectar_db
 import uuid
+import psycopg2.extras
 
 levelRoutes = Blueprint('levelRoutes', __name__)
 
-@levelRoutes.route('/nivel', methods=['POST', 'OPTIONS'])
-@cross_origin()
+@levelRoutes.route('/nivel', methods=['POST'])
 def guardar_puntaje():
-    if request.method == 'OPTIONS':
-        return '', 200  # Preflight CORS
-
     try:
         data = request.get_json()
         puntaje = data.get('puntaje')
         estudiante_id = data.get('student_id')
         nombre_estudiante = data.get('nombre')
 
-        # Generar UUID para v_id
         nivel_id = str(uuid.uuid4())
 
         conexion = conectar_db()
         cursor = conexion.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        # Usar v_id en lugar de id_level
         query = "INSERT INTO level (v_id, puntaje, student_id, v_name) VALUES (%s, %s, %s, %s)"
         cursor.execute(query, (nivel_id, puntaje, estudiante_id, nombre_estudiante))
         conexion.commit()
