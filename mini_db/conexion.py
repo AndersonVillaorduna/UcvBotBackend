82cec5565523fb061479125dccf48b6034a68209
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-06-26 00:59:13 -0500

    fix db postgre

--- mini_db/conexion.py
+++ mini_db/conexion.py
@@ -1,14 +1,19 @@
-import mysql.connector
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+
+load_dotenv()
 
 def conectar_db():
     try:
-        conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",  # tu contraseña si tienes
-            database="ucvbot"  # nombre exacto de tu BD
-        )
+        db_url = os.getenv("urldatabase")
+        if not db_url:
+            print("❌ Error: La variable de entorno 'urldatabase' no está configurada.")
+            return None
+
+        conexion = psycopg2.connect(db_url)
         return conexion
-    except mysql.connector.Error as e:
+    except Exception as e:
         print(f"❌ Error al conectar con la base de datos: {e}")
         return None
