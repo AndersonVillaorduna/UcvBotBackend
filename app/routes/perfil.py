bfd5bfc5c1ce7f93cf6c1fd411fe43343662d058
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-06-26 19:20:06 -0500

    fix import-not

--- app/routes/perfil.py
+++ app/routes/perfil.py
@@ -1,7 +1,6 @@
 import logging
 import traceback
 from flask import Blueprint, request, jsonify
-from flask_cors import cross_origin
 from mini_db.conexion import conectar_db
 import psycopg2.extras
 
@@ -9,11 +8,8 @@ logging.basicConfig(level=logging.INFO)
 
 perfil_bp = Blueprint('perfil_bp', __name__)
 
-@perfil_bp.route('/perfil', methods=['GET', 'PUT', 'OPTIONS'])
-@cross_origin()
+@perfil_bp.route('/perfil', methods=['GET', 'PUT'])
 def perfil():
-    if request.method == 'OPTIONS':
-        return '', 200
 
     if request.method == 'GET':
         user_uid = request.args.get('user_uid')
